4fb75234da3dbe5c8ffe38090c77ca5f7ff5daa3
HP <HP@LAPTOP-H15N5TDF>
2022-03-10 15:27:09 +0530

    Commit For First Time

---
+++ src/test/java/comCastGitDemo/createGitDemo.java
@@ -0,0 +1,13 @@
+package comCastGitDemo;
+
+import org.testng.annotations.Test;
+
+public class createGitDemo {
+	
+	@Test
+	public void gitDemo()
+	{
+		System.out.println("commit first");
+	}
+
+}
